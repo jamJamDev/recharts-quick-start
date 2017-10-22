8e44c4b53228ce9daa8abe215e9edb11fd8ae319
Jamie Thompson <jthompson@geo-jobe.com>
2017-10-22 13:38:04 -0500

    Added title

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SimpleLineChart from './SimpleLineChart.js';
+import image from './images/title.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Using Recharts</h1>
+          <img src={image} />
         </header>
-        <p className="App-intro">
-            Simple kickstart to any quick data visualizations I want to throw together using <a href="http://recharts.org/#/en-US">recharts</a>.
-        </p>
-          <SimpleLineChart></SimpleLineChart>
+      <div className="chart-area"><SimpleLineChart></SimpleLineChart></div>
       </div>
     );
   }
